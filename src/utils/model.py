fdaa3bba0942055dcdef5bb0011f6c6e6d56426c
i-ivanova <inna.ivanova.vr@gmail.com>
2021-04-15 22:12:05 -0700

    2 different attentions

--- src/utils/model.py
+++ src/utils/model.py
@@ -9,14 +9,30 @@ from transformers import BertTokenizer, BertModel
 from transformers import AutoTokenizer, AutoModel
 
 
+BERT_MODELS = {"multi" : {"model_name": "bert-base-multilingual-cased",
+                          "tokenizer": "bert-base-multilingual-cased"}, 
+               
+               "en" :    {"model_name": "bert-base-cased",
+                          "tokenizer": "bert-base-cased"},
+               
+               "de" :    {"model_name": "bert-base-german-cased",
+                          "tokenizer": "bert-base-german-cased"}}
+
 class language_encoder(nn.Module):
-    def __init__(self, model_path=None):
+    def __init__(self, model_path=None, model_type="multi"):
         super(language_encoder, self).__init__()
+        if model_type not in BERT_MODELS:
+            error_message = "Invalid BERT model {}. Supported BERT models: {}."
+            raise ValueError(error_message.format(model_type,
+                                                  ", ".join(list(BERT_MODELS.keys()))))
+            
+        model_name = BERT_MODELS[model_type]["model_name"]
+        tokenizer = BERT_MODELS[model_type]["tokenizer"]
+        
         if model_path is None:
-            self.model = BertModel.from_pretrained('DeepPavlov/bert-base-multilingual-cased-sentence',
-                                      output_hidden_states = True,
-                                      )
-            self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+            self.model = BertModel.from_pretrained(model_name, output_hidden_states=True)
+            self.tokenizer = BertTokenizer.from_pretrained(tokenizer)
+
         else:
             self.model = AutoModel.from_pretrained(model_path,
                                       output_hidden_states = True,
@@ -34,7 +50,7 @@ class TemporalAttention(nn.Module):
        Focuses on a particular joint frame in the sequence.
        Practically self-attention."""
 
-    def __init__(self, hidden_size):
+    def __init__(self, hidden_size, device="cuda"):
         super(TemporalAttention, self).__init__()
         self.hidden_size = hidden_size
         
@@ -44,7 +60,8 @@ class TemporalAttention(nn.Module):
         
         self.scaling_factor = torch.rsqrt(torch.tensor(self.hidden_size, dtype=torch.float))
         self.softmax = nn.Softmax(dim=2) 
-        self.neg_inf = torch.tensor(-1e7).cuda()
+        self.neg_inf = torch.tensor(-1e7).to(device)
+        self.device = device
         
         
     def forward(self, queries, keys, values):    
@@ -59,7 +76,8 @@ class TemporalAttention(nn.Module):
         # mask out the attention to the future decoder steps
         dec_seq = queries.shape[1]
         
-        mask = torch.tril(torch.ones((batch_size, dec_seq, dec_seq))).cuda()
+
+        mask = torch.tril(torch.ones((batch_size, dec_seq, dec_seq))).to(self.device)
         unnormalized_attention = unnormalized_attention.masked_fill(
             mask == 0, self.neg_inf)
 
@@ -73,17 +91,17 @@ class SpacialAttention(nn.Module):
        Focuses on particular joints in a single frame.
        """
     
-    def __init__(self, q_size, hidden_size):
+    def __init__(self, k_v_size, hidden_size):
         super(SpacialAttention, self).__init__()
         self.hidden_size = hidden_size
-        self.q_size = q_size
+        self.k_v_size = k_v_size
         self.softmax = nn.Softmax(dim=1)
 
-        self.Q = nn.Linear(q_size, q_size)
-        self.K = nn.Linear(hidden_size, q_size)
-        self.V = nn.Linear(hidden_size, q_size)
+        self.Q = nn.Linear(hidden_size, hidden_size)
+        self.K = nn.Linear(k_v_size, hidden_size)
+        self.V = nn.Linear(k_v_size, hidden_size)
         
-        self.scaling_factor = torch.rsqrt(torch.tensor(self.q_size, dtype=torch.float))
+        self.scaling_factor = torch.rsqrt(torch.tensor(self.hidden_size, dtype=torch.float))
         self.softmax = nn.Softmax(dim=2) 
 
         
@@ -97,7 +115,7 @@ class SpacialAttention(nn.Module):
         unnormalized_attention = torch.bmm(q, k.permute(0, 2, 1)) * self.scaling_factor
         attention_weights = self.softmax(unnormalized_attention)
         context = torch.bmm(attention_weights, v) 
-        return context, attention_weights
+        return context, attention_weights    
             
 
 class Encoder(nn.Module):
@@ -177,8 +195,143 @@ class DecoderCell(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, hidden_size, pose_size, trajectory_size,
                use_h=False, start_zero=False, use_tp=True,
-               use_lang=False, use_attn=False, num_layers=1):
+               use_lang=False, use_attn=False, num_layers=1, device="cuda"):
         super(Decoder, self).__init__()
+        self.input_size = pose_size + trajectory_size
+        self.cell = DecoderCell(hidden_size, pose_size, trajectory_size,
+                                use_h=use_h, use_tp=use_tp, use_lang=use_lang)
+        ## Hardcoded to reach 0% Teacher forcing in 20 epochs
+        self.tf = TeacherForcing(0.05)
+        self.start_zero = start_zero
+        self.use_lang = use_lang
+        self.use_attn = use_attn
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.kv_size = pose_size + int(use_lang) * self.hidden_size
+        self.dropout = nn.Dropout(0.1)
+
+        self.spacial_attention = nn.ModuleList([
+            SpacialAttention(k_v_size=self.kv_size, hidden_size=self.hidden_size) 
+            for i in range(self.num_layers)])
+        
+        self.temporal_attention = nn.ModuleList([
+            TemporalAttention(hidden_size=self.hidden_size, device=device) 
+            for i in range(self.num_layers)])
+        
+        self.attention_mlps = nn.ModuleList([
+          nn.Sequential(nn.Linear(self.hidden_size, self.hidden_size),
+                        nn.LeakyReLU()) for i in range(self.num_layers)])
+        
+        self.linear = nn.Linear(self.hidden_size, pose_size)
+
+    
+    def forward(self, h, time_steps, gt, epoch=np.inf, attn=None):
+        if self.use_lang:
+            lang_z = h
+            
+        if self.start_zero:
+            x = h.new_zeros(h.shape[0], self.input_size)
+            x = h.new_tensor(torch.rand(h.shape[0], self.input_size))
+        else:
+            x = gt[:, 0, :] ## starting point for the decoding 
+
+        Y = []
+        H = h.unsqueeze(1)
+        X = x.unsqueeze(1)
+        XH = torch.cat([X, H], dim=2)
+        for t in range(time_steps):
+            if self.use_attn:
+                if self.use_lang:
+                    x, h = self.cell(torch.cat([x, h], dim=-1), h)
+                    XH = torch.cat([XH, torch.cat(
+                        [x.unsqueeze(1), h.unsqueeze(1)], dim=1)], dim=1)
+                    H = torch.cat([H, h.unsqueeze(1)], dim=1)
+                    X = torch.cat([X, x.unsqueeze(1)], dim=1)
+
+                else:
+                    x, h = self.cell(x, h)
+                    H = torch.cat([H, h.unsqueeze(1)], dim=1)
+                    X = torch.cat([X, x.unsqueeze(1)], dim=1)
+
+            elif self.use_lang:
+                x, h = self.cell(torch.cat([x, h], dim=-1), h)
+            
+            else:
+                x, h = self.cell(x, h)
+                Y.append(x.unsqueeze(1))
+
+            if t > 0:
+                mask = self.tf(epoch, h.shape[0]).view(-1, 1).to(x.device)
+#                 mask = self.tf(epoch, h.shape[0]).double().view(-1, 1).to(x.device)
+                x = mask * gt[:, t-1, :] + (1-mask) * x
+
+        if self.use_attn:
+            if self.use_lang:
+                H = self.attention(self.dropout(H), XH)
+            else:
+                H = self.attention(self.dropout(H), X)
+            return self.linear(H)
+
+        return torch.cat(Y, dim=1)
+
+
+    def sample(self, h, time_steps, start, attn=None):
+        if self.use_lang:
+            lang_z = h
+
+        #x = torch.rand(h.shape[0], self.input_size).to(h.device).to(h.dtype)
+        x = start ## starting point for the decoding 
+        
+        Y = []
+        H = h.unsqueeze(1)
+        X = x.unsqueeze(1)
+        XH = torch.cat([X, H], dim=2)
+        for t in range(time_steps):
+            if self.use_attn:
+                if self.use_lang:
+                    x, h = self.cell(torch.cat([x, h], dim=-1), h)
+                    XH = torch.cat([XH, torch.cat(
+                        [x.unsqueeze(1), h.unsqueeze(1)], dim=1)], dim=1)
+                    H = torch.cat([H, h.unsqueeze(1)], dim=1)
+                    X = torch.cat([X, x.unsqueeze(1)], dim=1)
+
+                else:
+                    x, h = self.cell(x, h)
+                    H = torch.cat([H, h.unsqueeze(1)], dim=1)
+                    X = torch.cat([X, x.unsqueeze(1)], dim=1)
+
+            elif self.use_lang:
+                x, h = self.cell(torch.cat([x, h], dim=-1), h)
+            
+            else:
+                x, h = self.cell(x, h)
+                Y.append(x.unsqueeze(1))
+
+        if self.use_attn:
+            if self.use_lang:
+                H = self.attention(self.dropout(H), XH)
+            else:
+                H = self.attention(self.dropout(H), X)
+            return self.linear(H)
+
+        return torch.cat(Y, dim=1)
+ 
+
+    def attention(self, h, x):
+        for i in range(self.num_layers):
+            temporal_h, temporal_att_weights = self.temporal_attention[i](h, h, h)
+            h = temporal_h + h
+            spacial_h, spacial_att_weights = self.spacial_attention[i](h, x, x)
+            h = spacial_h + h
+            h = self.attention_mlps[i](h)
+        return h       
+
+    
+class DecoderAttJoints(nn.Module):
+    def __init__(self, hidden_size, pose_size, trajectory_size,
+               use_h=False, start_zero=False, use_tp=True,
+               use_lang=False, use_attn=False, num_layers=1, device="cuda"):
+        super(DecoderAttJoints, self).__init__()
         self.input_size = pose_size + trajectory_size
         self.cell = DecoderCell(hidden_size, pose_size, trajectory_size,
                                 use_h=use_h, use_tp=use_tp, use_lang=use_lang)
@@ -192,20 +345,22 @@ class Decoder(nn.Module):
         if use_attn:
             self.hidden_size += pose_size
         if use_lang:
-           self.hidden_size += hidden_size
+            self.hidden_size += hidden_size
         self.dropout = nn.Dropout(0.1)
 
         self.spacial_attention = nn.ModuleList([
-            SpacialAttention(q_size=self.hidden_size, hidden_size=hidden_size) 
+            SpacialAttention(k_v_size=hidden_size, hidden_size=self.hidden_size) 
             for i in range(self.num_layers)])
         
         self.temporal_attention = nn.ModuleList([
-            TemporalAttention(hidden_size=self.hidden_size) 
+            TemporalAttention(hidden_size=self.hidden_size, device=device) 
             for i in range(self.num_layers)])
         
         self.attention_mlps = nn.ModuleList([
           nn.Sequential(nn.Linear(self.hidden_size, self.hidden_size),
                         nn.ReLU()) for i in range(self.num_layers)])
+        
+        self.linear = nn.Linear(self.hidden_size, pose_size)
 
     
     def forward(self, h, time_steps, gt, epoch=np.inf, attn=None):
@@ -251,7 +406,7 @@ class Decoder(nn.Module):
                 x = mask * gt[:, t-1, :] + (1-mask) * x
 
         if self.use_attn:
-            return X
+            return self.linear(X)
 
         return torch.cat(Y, dim=1)
 
@@ -291,7 +446,7 @@ class Decoder(nn.Module):
                 Y.append(x.unsqueeze(1))
 
         if self.use_attn:
-            return X
+            return self.linear(X)
 
         return torch.cat(Y, dim=1)
         
@@ -299,13 +454,11 @@ class Decoder(nn.Module):
         for i in range(self.num_layers):
             temporal_x, temporal_att_weights = self.temporal_attention[i](x, x, x)
             x = temporal_x + x
-            # last_frame_x = x[:, -1, :]
-            # spacial_x, spacial_att_weights = self.spacial_attention[i](last_frame_h, h, h)
             spacial_x, spacial_att_weights = self.spacial_attention[i](x, h, h)
             x = spacial_x + x
             x = self.attention_mlps[i](x)
         return x
-    
+
 
 class Seq2Seq(nn.Module):
     def __init__(self, hidden_size, pose_size, trajectory_size,
